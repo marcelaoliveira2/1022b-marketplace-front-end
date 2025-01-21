97707aafa410598da75b9dbe260c9f1f35f95888
marcelaoliveira2 <163016115+marcelaoliveira2@users.noreply.github.com>
2025-01-21 07:03:15 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
 import './App.css'
 
 type ProdutoType = {
   id:number,
   nome:string,
   descricao:string,
-  valor:string,
+  preco:string,
   imagem:string
 }
 function App() {
@@ -16,6 +17,21 @@ function App() {
     .then(resposta=>resposta.json())
     .then(dados=>setProdutos(dados))
   },[])
+  function handleExcluir(id:number){
+    fetch(`http://localhost:8000/produtos/${id}`,{
+      method:"DELETE"
+    })
+    .then(resposta=>{
+      if(resposta.status==200){
+        alert("Excluído com sucesso")
+        window.location.reload()
+      }
+      else{
+        alert("Erro ao excluir")
+      }
+    })
+  }
+
   return (
     <>  
       <div className="container-produtos">
@@ -24,8 +40,10 @@ function App() {
             <div key={prod.id} className="produto-item">
               <h1>{prod.nome}</h1>
               <img src={prod.imagem} alt="Imagem de celular" />
-              <p>R$: {prod.valor}</p>
+              <p>{prod.preco}</p>
               <p>{prod.descricao}</p>
+              <button onClick={()=>{handleExcluir(prod.id)}}>Excluir</button>
+              <Link to={`/alterar-produto/${prod.id}`}>Alterar</Link>
             </div>
           )
         })}
