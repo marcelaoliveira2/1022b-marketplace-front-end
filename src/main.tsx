264ba9a155523c390fd0767b90e0985383c17e2a
marcelaoliveira2 <163016115+marcelaoliveira2@users.noreply.github.com>
2025-01-22 07:45:08 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { createBrowserRouter, RouterProvider, } from "react-router-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import CadastroProduto from './componentes/cadastroproduto/CadastroProduto.tsx';
+import AlterarProduto from './componentes/alterarproduto/AlterarProduto.tsx';
 
+//Crie um comentário explicando o que faz o código abaixo
+//Cria uma instância do BrowserRouter e passa um array de objetos com as rotas da aplicação
 const router = createBrowserRouter([
   {
     path: "/",
